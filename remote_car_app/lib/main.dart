fb7b60186aa94947e1bc3966f3d53812c4d026e3
Gabriel <ilie.cgabriel@gmail.com>
2022-05-22 18:21:18 +0300

    Added scanning via bluetooth functionality

--- remote_car_app/lib/main.dart
+++ remote_car_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,20 +17,44 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget{
-  const MyHomePage({Key? key}) : super(key: key);
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key}) : super(key: key);
 
+// ignore: prefer_const_constructors
+  final a = FlutterBlue.instance.startScan(scanMode: ScanMode.lowLatency);
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello'),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: StreamBuilder(
+            stream: FlutterBlue.instance.scanResults,
+            builder: (_, AsyncSnapshot<List<ScanResult>> snapshot) =>
+                ListView.builder(
+              itemCount: snapshot.data?.length,
+              itemBuilder: (_, int index) {
+                if (snapshot.data == null || snapshot.data!.isEmpty) {
+                  final state = snapshot.connectionState.name;
+                  return Text(
+                    '$state empty',
+                    key: const Key('1'),
+                  );
+                }
+                final title = snapshot.data?[index].device.name ?? 'no name';
+                return Text(
+                  title,
+                  key: Key(snapshot.data![index].device.id.id),
+                );
+              },
+            ),
+          ),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
